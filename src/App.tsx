2448db49bdde7d741435093b2e2b3e925d19d98f
bineta66 <binetabadiane6gmail.com>
2025-10-11 15:41:44 +0000

    components

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import Login from "./components/login";
 
 import Register from "./components/Register";
-import Home from "./components/Homepage";
 import Troubles from "./components/Troubles";
 import Temoignages from "./components/Temoignages";
 import ProfessionalsList from "./components/ProfessionalsList";
 import Articles from "./components/Articles";
 import ShareExperience from "./components/ShareExperience";
+import Homepage from "./components/Homepage";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(() => !!localStorage.getItem('user'));
@@ -25,14 +25,20 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
-        <Route path="/register" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Register />} />
-        <Route path="/home" element={isAuthenticated ? <Homepage /> : <Navigate to="/" />} />
+        <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/register" element={isAuthenticated ? <Navigate to="/home" /> : <Register />} />
+        
         
         {/* Cette route doit toujours être accessible */}
         <Route path="/professionals" element={<ProfessionalsList />} />
         <Route path="/share-experience" element={<ShareExperience />} />
         <Route path="/test" element={<h1>Test OK</h1>} />
+        <Route path="/troubles" element={<Troubles />} />
+        <Route path="/temoignages" element={<Temoignages />} />
+        <Route path="/articles/:id" element={<Articles />} />
+        <Route path="/home" element={<Homepage />} />
+
+        
       </Routes>
     </Router>
   );
